8f47db19f898806b2de85c391b0cae737455e394
avan989 <avan989@yahoo.com>
2017-07-16 01:16:02 -0400

    Initial Commit

---
+++ Easy Meme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easy Meme
+//
+//  Created by Anh Duc Van on 7/16/17.
+//  Copyright © 2017 Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
